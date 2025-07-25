971a0d050ecbf78864bc41849c5ea5e83d1dc350
Prasanth Tata <prashtata@gmail.com>
2025-07-24 17:19:03 -0700

    Rehaul using only html

--- src/components/Projects.jsx
+++
@@ -1,5 +0,0 @@
-// src/components/About.jsx
-import React from 'react';
-export default function Projects() {
-  return <div>Projects: This thing that looking at rn</div>;
-}
--- src/components/Skills.jsx
+++
@@ -1,5 +0,0 @@
-// src/components/About.jsx
-import React from 'react';
-export default function Skills() {
-  return <div>Skills: dad-joke prompt engineering</div>;
-}
